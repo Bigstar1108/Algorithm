a9310a96fb6f68c6b6752153e7285ade9bd740b4
Daeseong Hwang <65744080+Bigstar1108@users.noreply.github.com>
2024-08-08 18:28:21 +0900

    [goormlevel] Title: 예산, Time: 1.39 ms, Memory: 117.08 MB, Difficulty: 0 -BaekjoonHub

---
+++ goormlevel/242879/1. 예산/예산.dart
@@ -0,0 +1,31 @@
+import 'dart:io';
+void main() {
+	var line = stdin.readLineSync()!;
+	
+	// 구입할 물품의 종류 수
+	var totalProductTypesCount = int.parse(line.split(' ')[0]);
+	
+	// 예산
+	var budget = int.parse(line.split(' ')[1]);
+	
+	// 구매할 물품의 가격 및 구매 개수를 가지고 있는 라인
+	List<String> productPriceAndAmountLines = [];
+	
+	for (int i = 0; i < totalProductTypesCount; i++) {
+		var line = stdin.readLineSync()!;
+		productPriceAndAmountLines.add(line);
+	}
+	
+	for (var line in productPriceAndAmountLines) {
+		var price = int.parse(line.split(' ')[0]);
+		var amount = int.parse(line.split(' ')[1]);
+		
+		budget -= price * amount;
+	}
+	
+	if (budget < 0) {
+		print('No');
+	} else {
+		print(budget);
+	}
+}
